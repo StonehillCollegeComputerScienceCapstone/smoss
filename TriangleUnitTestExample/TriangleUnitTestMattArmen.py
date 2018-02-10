28324fc7baefd3f96f0295c5704e4b574c55fee8
myoung95 <myoung2@students.stonehill.edu>
2018-02-09 20:15:57 -0500

    Renamed files and commited changes from Friday's class

--- TriangleUnitTestExample/TriangleUnitTestMattArmen.py
+++ TriangleUnitTestExample/TriangleUnitTestMattArmen.py
@@ -1,27 +1,27 @@
 import unittest
-from TriangleMNA import TriangleMNA
+from TriangleMattArmen import TriangleMattArmen
 
 class TriangleUnitTestMNA(unittest.TestCase):
 
     # Tests if all input values are ints
     def test_isValidInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertTrue(t.isValidInput(1, 1, 1))
 
     # Tests if all input values are not strings
     def test_isStringInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertFalse(t.isValidInput('hello', 'world', 'string'))
         self.assertFalse(t.isValidInput(' ', ' ', ' '))
 
     # Tests if all input values are not doubles
     def test_isDoubleInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertFalse(t.isValidInput(3.14, 2.718, 0.5))
 
     # Tests if all input values are not strings
     def test_isNoneInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertFalse(t.isValidInput(None, None, None))
         self.assertFalse(t.isValidInput(1, None, None))
         self.assertFalse(t.isValidInput(None, 1, None))
@@ -32,17 +32,17 @@ class TriangleUnitTestMNA(unittest.TestCase):
 
     # Tests if all input values are not negative
     def test_isNegativeInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertFalse(t.isValidInput(-1, -2, -3))
 
     # Tests if all input values are not zero
     def test_isZeroInput(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertFalse(t.isValidInput(0, 0, 0))
 
     # Tests if all input values are different
     def test_isScalene(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertTrue(t.isScalene(2, 3, 4))
         self.assertFalse(t.isScalene(2, 2, 3))
         self.assertFalse(t.isScalene(2, 3, 2))
@@ -51,7 +51,7 @@ class TriangleUnitTestMNA(unittest.TestCase):
 
     # Tests if any two input values are the same
     def test_isIsosceles(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertTrue(t.isIsosceles(2, 2, 3))
         self.assertTrue(t.isIsosceles(2, 3, 2))
         self.assertTrue(t.isIsosceles(3, 2, 2))
@@ -60,7 +60,7 @@ class TriangleUnitTestMNA(unittest.TestCase):
 
     # Tests if all input values are the same
     def test_isEquilateral(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertTrue(t.isEquilateral(1, 1, 1))
         self.assertFalse(t.isEquilateral(2, 3, 4))
         self.assertFalse(t.isEquilateral(2, 2, 3))
@@ -69,7 +69,7 @@ class TriangleUnitTestMNA(unittest.TestCase):
 
     # Triangle inequality theorem
     def test_isValidTriangle(self):
-        t = TriangleMNA()
+        t = TriangleMattArmen()
         self.assertTrue(t.isValidTriangle(3, 2, 2))
         self.assertTrue(t.isValidTriangle(2, 3, 2))
         self.assertTrue(t.isValidTriangle(2, 2, 3))
