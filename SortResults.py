56d052f2c53fbf320ed4a982ea5bde3e2538ddc9
victoriamoeckel <victoriamoeckel@gmail.com>
2018-02-13 12:26:01 -0500

    Sorting MOSS results unit tests

--- SortResults.py
+++ SortResults.py
@@ -1,9 +1,10 @@
 class SortResults:
 
-    fileName = "MOSSresults.csv"
+    def __init__(self):
+        self.fileName = "MOSSinput.csv"
 
     def isValidFilename(self):
+        self.fileName = self.fileName.lower()
+        if self.fileName.endswith('.csv'):
+            return True
         return False
-
-    def isEmptyFile(self):
-        return True
--- SortResultsUnitTest.py
+++ SortResultsUnitTest.py
@@ -3,31 +3,25 @@ from SortResults import SortResults
 
 class MyTestCase(unittest.TestCase):
 
-    def setUpClass(self):
-        self.sr = SortResults()
+    def testValidInputFileLower(self):
+        sr = SortResults()
+        sr.fileName = "MOSSresults.csv"
+        self.assertTrue(sr.isValidFilename())
 
-    def testValidInputFilecsv(self):
-        self.fileName = "MOSSresults.csv"
-        self.assertTrue(self.isValidFilename())
-
-    def testValidInputFileCSV(self):
-        self.fileName = "MOSSresults.CSV"
-        self.assertTrue(self.isValidFilename())
-
-    def testValidInputFileXLS(self):
-        self.fileName = "MOSSresults.XLS"
-        self.assertFalse(self.isValidFilename())
+    def testValidInputFileUpper(self):
+        sr = SortResults()
+        sr.fileName = "MOSSresults.CSV"
+        self.assertTrue(sr.isValidFilename())
 
     def testValidInputFileXLS(self):
-        self.fileName = "MOSSresults.XLSX"
-        self.assertFalse(self.isValidFilename())
+        sr = SortResults()
+        sr.fileName = "MOSSresults.XLS"
+        self.assertFalse(sr.isValidFilename())
 
-    def testEmptyFile(self):
-        self.fileName = "MOSSresults.csv"
-        self.assertFalse(self.isEmptyFile())
-
-    def tearDownClass(cls):
-        print("Tests complete")
+    def testValidInputFileXLSX(self):
+        sr = SortResults()
+        sr.fileName = "MOSSresults.XLSX"
+        self.assertFalse(sr.isValidFilename())
 
     if __name__ == '__main__':
         unittest.main()
