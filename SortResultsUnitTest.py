b00fbc76fad0712910a088239016894a80888276
victoriamoeckel <victoriamoeckel@gmail.com>
2018-02-13 15:47:37 -0500

    Sorting MOSS results unit tests

--- SortResultsUnitTest.py
+++ SortResultsUnitTest.py
@@ -5,23 +5,41 @@ class MyTestCase(unittest.TestCase):
 
     def testValidInputFileLower(self):
         sr = SortResults()
-        sr.fileName = "MOSSresults.csv"
+        sr.inputFileName = "MOSSresults.csv"
         self.assertTrue(sr.isValidFilename())
+
+    def testValidInputFileJustExtension(self):
+        sr = SortResults()
+        sr.inputFileName = ".csv"
+        self.assertFalse(sr.isValidFilename())
 
     def testValidInputFileUpper(self):
         sr = SortResults()
-        sr.fileName = "MOSSresults.CSV"
+        sr.inputFileName = "MOSSresults.CSV"
         self.assertTrue(sr.isValidFilename())
 
     def testValidInputFileXLS(self):
         sr = SortResults()
-        sr.fileName = "MOSSresults.XLS"
+        sr.inputFileName = "MOSSresults.XLS"
         self.assertFalse(sr.isValidFilename())
 
     def testValidInputFileXLSX(self):
         sr = SortResults()
-        sr.fileName = "MOSSresults.XLSX"
+        sr.inputFileName = "MOSSresults.XLSX"
         self.assertFalse(sr.isValidFilename())
+
+    def testMainList(self):
+        sr = SortResults()
+        sr.inputFileName = "MOSSinput.csv"
+        sr.createMainList()
+        self.assertEqual(3, len(sr.MOSSresults))
+
+    def testMainListBadFile(self):
+        sr = SortResults()
+        sr.inputFileName = "MOSSinput.csv"
+        self.assertTrue(sr.createMainList())
+
+
 
     if __name__ == '__main__':
         unittest.main()
