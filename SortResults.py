3de2babf1ace819afdce669de1f415d85ba37a5f
victoriamoeckel <victoriamoeckel@gmail.com>
2018-02-13 11:57:30 -0500

    Sorting MOSS results unit tests

---
+++ SortResults.py
@@ -0,0 +1,9 @@
+class SortResults:
+
+    fileName = "MOSSresults.csv"
+
+    def isValidFilename(self):
+        return False
+
+    def isEmptyFile(self):
+        return True
--- SortResultsUnitTest.py
+++ SortResultsUnitTest.py
@@ -1,8 +1,33 @@
 import unittest
+from SortResults import SortResults
 
 class MyTestCase(unittest.TestCase):
 
-    
+    def setUpClass(self):
+        self.sr = SortResults()
+
+    def testValidInputFilecsv(self):
+        self.fileName = "MOSSresults.csv"
+        self.assertTrue(self.isValidFilename())
+
+    def testValidInputFileCSV(self):
+        self.fileName = "MOSSresults.CSV"
+        self.assertTrue(self.isValidFilename())
+
+    def testValidInputFileXLS(self):
+        self.fileName = "MOSSresults.XLS"
+        self.assertFalse(self.isValidFilename())
+
+    def testValidInputFileXLS(self):
+        self.fileName = "MOSSresults.XLSX"
+        self.assertFalse(self.isValidFilename())
+
+    def testEmptyFile(self):
+        self.fileName = "MOSSresults.csv"
+        self.assertFalse(self.isEmptyFile())
+
+    def tearDownClass(cls):
+        print("Tests complete")
 
     if __name__ == '__main__':
         unittest.main()
