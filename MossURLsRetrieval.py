5cca3a9b304ad0570d3d6ccfecd2f0c562845105
Nick Lisichenok <nlisichenok@students.stonehill.edu>
2018-02-14 12:46:30 -0500

    MossParser class and unit test

--- MossURLsRetrieval.py
+++ MossURLsRetrieval.py
@@ -1,4 +1,8 @@
 from Result import Result
+import urllib
+import urllib.request, urllib.error
+import html.parser
+import sys
 
 class MossURLsRetrieval:
     def __init__(self):
@@ -9,6 +13,20 @@ class MossURLsRetrieval:
     def getURL(self, url):
         #check to see if it has "moss.stanford.edu"
         #check that it exists (http response of 200?)
+        if (not isinstance(url, str)):
+            return False
+        if ("moss.stanford.edu/results" not in url): #this can be changed by Stanford at any time
+            return False
+        if (url in self.urls): #URL already exists in list
+            return False
+        try:
+            connection = urllib.request.urlopen(url)
+        except urllib.error.HTTPError as e: #case of 404 Not Found
+            print(e)
+            return False
+        except urllib.error.URLError as e: #case connection refused
+            print(e)
+            return False
         self.urls.append(url)
         return True
 
--- MossURLsRetrievalUnitTest.py
+++ MossURLsRetrievalUnitTest.py
@@ -11,10 +11,11 @@ class MossURLsTests(unittest.TestCase):
 
     def test_InvalidURL(self):
         self.assertFalse(self.MossURLsData.getURL("notaURL"))  # this is not a valid URL
+        self.assertFalse(self.MossURLsData.getURL(1)) #in case reading in a file can go wrong leading to this point
+        self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/121212121212121212112/")) #in case invalid moss URL
 
     def test_Invalid_sameURL(self):
         self.assertTrue(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))
-
         #trying to get the information from the same URL will fail
         self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))
 
@@ -28,3 +29,9 @@ class MossURLsTests(unittest.TestCase):
 
     def tearDown(self):
         self.MossURLsData = None
+
+def main():
+    print("Start")
+
+if __name__ == '__main__':
+    unittest.main()
