dffcf287fdc56102f35790293cafaa54ff9aa5f9
Matt Peters <mpeters@students.stonehill.edu>
2018-02-13 21:23:22 -0500

    MossParser class and unit test

---
+++ MossParser.py
@@ -0,0 +1,25 @@
+#Class MossParser
+#This class is designed to take a url as input and creates a csv file of results from MOSS in the same directory
+#as this program
+
+
+class MossParser:
+
+    #Grab an input from the user
+
+    #If this is not a valid URL display error and exit
+
+    #get the html text from the URL
+    #-> html = getHtml(some_url)
+
+    #Process the html into table strings
+    #-> tableStrings = processHtml(html)
+
+    #Process the table strings into csv strings
+    #-> csvStrings = processTableStrings(tableStrings)
+
+    #Create, save, and close the csv file
+
+
+
+
