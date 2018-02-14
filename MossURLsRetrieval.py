b9938eeb7c8eca66d3969a2660318f016264a07b
Sam Bradford <Bradford@Sams-MacBook-Pro-2.local>
2018-02-13 20:34:06 -0500

    Start of adding the Multiple moss URL retrievals

---
+++ MossURLsRetrieval.py
@@ -0,0 +1,24 @@
+
+
+class MossURLsRetrieval:
+    def __init__(self):
+        self.urls = []
+        self.html = ""
+        #potentially a data variable?
+
+    def getURL(self, url):
+        #check to see if it has "moss.stanford.edu"
+        #check that it exists (http response of 200?)
+        self.urls.append(url)
+        return True
+
+    def gatherData(self):
+        for url in self.urls:
+            self.parseHTML(url)
+
+    def parseHTML(self, url):
+        #take url, send request for html
+        #take html, parse that it is correctly formated
+        #maybe break up the data?
+        #otherwise, append to html string
+        return True
