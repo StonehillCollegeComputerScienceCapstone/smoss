46b484b7df1877c881b04afbedc4aee2648c4c84
Nick Lisichenok <nlisichenok@students.stonehill.edu>
2018-02-14 13:50:32 -0500

    MossURLsRetrieval class and unit test

--- test/MossURLsRetrievalUnitTest.py
+++ test/MossURLsRetrievalUnitTest.py
@@ -1,7 +1,6 @@
 import unittest
 from MossURLsRetrieval import MossURLsRetrieval
 
-
 class MossURLsTests(unittest.TestCase):
     def setUp(self):
         self.MossURLsData = MossURLsRetrieval()
@@ -12,7 +11,7 @@ class MossURLsTests(unittest.TestCase):
     def test_InvalidURL(self):
         self.assertFalse(self.MossURLsData.getURL("notaURL"))  # this is not a valid URL
         self.assertFalse(self.MossURLsData.getURL(1)) #in case file reading goes wrong to this
-        self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/12121212121212/"))
+        self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/12121212121212/")) #404 not found
 
     def test_Invalid_sameURL(self):
         self.assertTrue(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))
@@ -24,7 +23,6 @@ class MossURLsTests(unittest.TestCase):
 
 def main():
     print("Start")
-
 
 if __name__ == '__main__':
     unittest.main()
