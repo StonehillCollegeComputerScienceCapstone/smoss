0b5e41322081d282593418af3ac212253aa6bb6a
Nick Lisichenok <nlisichenok@students.stonehill.edu>
2018-02-15 20:26:15 -0500

    MossURLsRetrieval class and unit test

--- test/MossURLsRetrievalUnitTest.py
+++ test/MossURLsRetrievalUnitTest.py
@@ -1,28 +1,42 @@
 import unittest
 from MossURLsRetrieval import MossURLsRetrieval
 
+
 class MossURLsTests(unittest.TestCase):
     def setUp(self):
         self.MossURLsData = MossURLsRetrieval()
 
-    def test_ValidURL(self):
-        self.assertTrue(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))  # valid URL
+    def test_Valid_URL(self):
+        self.assertTrue(self.MossURLsData.get_url("http://moss.stanford.edu/results/299782671/"))  # valid URL
 
-    def test_InvalidURL(self):
-        self.assertFalse(self.MossURLsData.getURL("notaURL"))  # this is not a valid URL
-        self.assertFalse(self.MossURLsData.getURL(1)) #in case file reading goes wrong to this
-        self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/12121212121212/")) #404 not found
+    def test_Invalid_URL(self):
+        self.assertFalse(self.MossURLsData.get_url("notURL"))  # this is not a valid URL
+        self.assertFalse(self.MossURLsData.get_url(1))  # in case file reading goes wrong to this
+        self.assertFalse(self.MossURLsData.get_url("http://moss.stanford.edu/results/12121212121212/"))  # 404 not found
 
-    def test_Invalid_sameURL(self):
-        self.assertTrue(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))
-        #trying to get the information from the same URL will fail
-        self.assertFalse(self.MossURLsData.getURL("http://moss.stanford.edu/results/299782671/"))
+    def test_Invalid_Same_URL(self):
+        self.assertTrue(self.MossURLsData.get_url("http://moss.stanford.edu/results/299782671/"))
+        # trying to get the information from the same URL will fail
+        self.assertFalse(self.MossURLsData.get_url("http://moss.stanford.edu/results/299782671/"))
+
+    def test_Read_URLs_From_Valid_File(self):
+        self.assertTrue(self.MossURLsData.get_file_urls("FileInput.txt"))
+
+    def test_Read_URLs_From_Invalid_File(self):
+        self.assertFalse(self.MossURLsData.get_file_urls("Invalid"))
+        self.assertFalse(self.MossURLsData.get_file_urls(1))
+
+    def test_Number_of_Valid_URLs_Read(self):
+        self.MossURLsData.get_file_urls("FileInput.txt")
+        self.assertEqual(len(self.MossURLsData.urls), 5)
 
     def tearDown(self):
         self.MossURLsData = None
 
+
 def main():
     print("Start")
+
 
 if __name__ == '__main__':
     unittest.main()
