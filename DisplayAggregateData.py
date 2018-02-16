de3f765ec003483e57466ba873740d0864e05577
Sam Bradford <Bradford@Sams-MacBook-Pro-2.local>
2018-02-15 22:26:53 -0500

    editted display tests

--- DisplayAggregateData.py
+++ DisplayAggregateData.py
@@ -9,17 +9,23 @@ class DisplayAggregateData:
     def set_Address(self, addr):
         if addr is not None: #not null
             self.address = addr
+            return True
+        else:
+            return False
 
-    def formHTML(self):
+    def formHTML(self, object):
         #take 'result' objects / aggregate data, form html
-
-        return self.html
+        self.html = object
+        return True
 
     def post(self):
         return True
         #take html and sent to browser
 
 
-    def print(self):
+    def print_to_file(self, file_name):
+        file = open(file_name, "w")
+        file.write(self.html)
+        file.close()
         return True
         #print to file the aggregate data
