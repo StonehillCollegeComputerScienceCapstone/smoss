e02e4a6f6edc3365af8f1e4a7028aaa5ab9a86e6
Sam Bradford <Bradford@Sams-MacBook-Pro-2.local>
2018-02-15 21:59:54 -0500

    display aggregate data

---
+++ test/DisplayAggregateDataTest.py
@@ -0,0 +1,21 @@
+import unittest
+from DisplayAggregateData import DisplayAggregateData
+
+
+class MossURLsTests(unittest.TestCase):
+    def setUp(self):
+        self.Display = DisplayAggregateData()
+
+    def test_invalid_Address(self):
+        self.Display.set
+
+    def tearDown(self):
+        self.Display = None
+
+
+def main():
+    print("Start")
+
+
+if __name__ == '__main__':
+    unittest.main()
