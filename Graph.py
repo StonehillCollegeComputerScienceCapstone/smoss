49012c15aaf0ca3d59e5ac12e887c01074eeeaec
victoriamoeckel <victoriamoeckel@gmail.com>
2018-02-27 15:56:54 -0500

    Beginning class for back end graph

--- Graph.py
+++ Graph.py
@@ -3,16 +3,24 @@ from AggregateData import AggregateData
 
 class Graph:
 
+    def __init__(self):
+
     def print(self):
         print('print')
 
+    def cmp(self, a, b):
+        return (a > b) - (a < b)
+
     def getNames(self):
+        return False
+
+    def pairNames(self, names):
         return False
 
 
 def main():
     names = ['Matt', 'Tori', 'Will']
-    g = nx.DiGraph()
-    g.add_edge(names[1], names[2], weight=80)
+    #g = nx.DiGraph()
+    #g.add_edge(names[1], names[2], weight=80)
 
 if __name__ == '__main__': main()
