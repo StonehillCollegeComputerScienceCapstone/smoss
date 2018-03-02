a757762cdec91d81af5c61353bbcda56a1af21d6
Michael M <Mpm1996@gmail.com>
2018-03-02 00:55:09 -0500

    CI Environment Update Configuration - Pt. 4
    - Removed binary for BrowserStackLocal (It's installed in Travis with the YML file...)
    - Updated YML file to run the server in the background
    - Removed needless variable 'app' in SingleURLSubmissionTest

--- test/frontend/SingleURLSubmissionTest.py
+++ test/frontend/SingleURLSubmissionTest.py
@@ -14,7 +14,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class SingleURLSubmissionTest (unittest.TestCase):
-    app = None
     url = None
     driver = None
 
