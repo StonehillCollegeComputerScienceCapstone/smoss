86d85fd541ec1f48f58ae343f5e339fc844aee69
myoung95 <myoung2@students.stonehill.edu>
2018-03-16 01:38:30 -0400

    Began renaming variables to camel case

--- Result.py
+++ Result.py
@@ -1,12 +1,12 @@
 class Result:
-    def __init__(self, assignment_num, f1, f2, url, f1_percent, f2_percent, lines_matched):
-        self.assignment_number = assignment_num
+    def __init__(self, assignmentNum, f1, f2, url, f1Percent, f2Percent, linesMatched):
+        self.assignment_number = assignmentNum
         self.file_one = f1
         self.file_two = f2
         self.url = url
-        self.file_one_percent = f1_percent
-        self.file_two_percent = f2_percent
-        self.lines_matched = lines_matched
+        self.file_one_percent = f1Percent
+        self.file_two_percent = f2Percent
+        self.lines_matched = linesMatched
 
     def get_assignment_number(self):
         return self.assignment_number()
