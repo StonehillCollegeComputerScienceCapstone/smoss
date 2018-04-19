360e703da9bc67f64a9c9a2491918de68e480918
Matt Peters <mpeters@students.stonehill.edu>
2018-04-19 17:55:41 -0400

    fixed getMossUrls.sh from producing mutliple copies of same file

--- getMossUrls.sh
+++ getMossUrls.sh
@@ -3,79 +3,82 @@
 function main
 {
 	# Array of assignment names and empty URL array
-	assignments=( "Warmup" "TwentyOne" "SquareRoot" "Insipid" "Rodentia" )
+	assignments=( "magicsquare" "palindrome" "twentyone")
 	
 	# cd to data directory
-	cd data
+	cd newData
 	
 	# For each assignment in the array, run MOSS and store the results
-	#for assignment in "${assignments[@]}"
-	#do
-	#	echo "Retrieving $assignment..."
-	#	mossResults=$(perl ../moss.pl -l java *"$assignment".java)
-	#	url=$(echo $mossResults | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*') # Search the results for the regular expression
-	#	echo "$url"
-	#	urls+=($url)
-	#done
+	for assignment in "${assignments[@]}"
+	do
+		echo "Retrieving $assignment..."
+		mossResults=$(perl ../moss.pl -l java *"$assignment".java)
+		url=$(echo $mossResults | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*') # Search the results for the regular expression
+		echo "$url"
+		urls+=($url)
+	done
 
 
 	#URLs for testing
 
 
 	echo "Retrieving url for test_processTableStrings1"
-	test1=$(perl ../moss.pl -l java jbaxter5_Warmup.java)
+	test1=$(perl ../moss.pl -l java abhatia_magicsquare.java)
 	url=$(echo $test1 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
 	echo "$url"
 	#echo "\n"
 	testurls+=($url)
 
-	echo "Retrieving url for test_processTableStrings2"
-	test2=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Insipid.java stentacles_Warmup.java stentacles_Insipid.java)
-	url=$(echo $test2 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
-	echo "$url"
+	#echo "Retrieving url for test_processTableStrings2"
+	#test2=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Insipid.java stentacles_Warmup.java stentacles_Insipid.java)
+	#url=$(echo $test2 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
+	#echo "$url"
 	#echo "\n"
-	testurls+=($url)
+	#testurls+=($url)
 
-    echo "Retrieving url for test_processTableStrings3"
-    test3=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Warmup.java)
-    url=$(echo $test3 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
-    echo $url
+    #echo "Retrieving url for test_processTableStrings3"
+    #test3=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Warmup.java)
+    #url=$(echo $test3 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
+    #echo $url
     #echo "\n"
-    testurls+=($url)
+    #testurls+=($url)
 
-    echo "Retrieving url for test_processTableStrings4"
-    test4=$(perl ../moss.pl -l java jbaxter5_Warmup.java stentacles_Warmup.java jbaxter5_Insipid.java stentacles_Insipid.java )
-    url=$(echo $test4 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
-    echo $url
+    #echo "Retrieving url for test_processTableStrings4"
+    #test4=$(perl ../moss.pl -l java jbaxter5_Warmup.java stentacles_Warmup.java jbaxter5_Insipid.java stentacles_Insipid.java )
+    #url=$(echo $test4 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
+    #echo $url
     #echo "\n"
-    testurls+=($url)
+    #testurls+=($url)
 
-    echo "Retrieving url for test_processTableStrings5"
-    test5=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Warmup.java)
-    url=$(echo $test5 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
-    echo $url
+    #echo "Retrieving url for test_processTableStrings5"
+    #test5=$(perl ../moss.pl -l java jbaxter5_Warmup.java jbaxter5_Warmup.java)
+    #url=$(echo $test5 | grep -o 'http:\/\/moss\.stanford\.edu\/results\/[0-9][0-9]*')
+    #echo $url
     #echo "\n"
-    testurls+=($url)
+    #testurls+=($url)
 
 
 	#cd back to smoss directory
 	cd ..
 	
 	# Write all URLs to mossUrls.txt
-	#for url in "${urls[@]}"
-	#do
-	#	echo "$url"
-	#done > mossUrls.txt
+	for url in "${urls[@]}"
+	do
+		echo "$url"
+	done > mossUrls.txt
 
 	# Write test urls to testurls.txt
 	for url in "${testurls[@]}"
 	do
 	    echo "$url"
 	done > testurls.txt
-	cp testurls.txt test/testurls.txt
 	
 	# Copy mossUrls.txt to the test directory
 	cp mossUrls.txt test/mossUrls.txt
+    rm mossUrls.txt
+	#Move testurls to test directory
+	cp testurls.txt test/testurls.txt
+	rm testUrls.txt
 }
 
 main
