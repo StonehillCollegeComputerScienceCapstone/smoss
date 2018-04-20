ae9c1588783061719d8fce2e332e1cc5e636c82f
Matt Peters <mpeters@students.stonehill.edu>
2018-04-19 21:58:10 -0400

    refactor to MossParser

--- MossParser.py
+++ MossParser.py
@@ -114,8 +114,7 @@ class MossParser ():
 
     def processTableStrings(self, tableStrings):
         # Go through list and turn them into Result object
-        csvStrings = []
-        csvPreviousStrings = []
+        csvStrings = csvPreviousStrings = []
         previousSet = set()
         for tableString in tableStrings:
             tableStringValues=self.getTableStringValues(tableString)
