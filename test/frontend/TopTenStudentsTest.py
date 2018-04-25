0dedd5992cc835c3d02de2216c133d125bfefd96
Michael M <Mpm1996@gmail.com>
2018-04-25 00:16:12 -0400

    Added additional FrontEnd tests
    Added the following tests:
    - InvalidURL submission
    - MultipleURLs ProvidedAllChosen
    
    Modified the following tests:
    - SingleURLCheckingAllURLsButton

--- test/frontend/TopTenStudentsTest.py
+++ test/frontend/TopTenStudentsTest.py
@@ -44,6 +44,31 @@ class TopTenStudentsTest (FrontEndConfig.FrontEndTestSuite):
 
 
     #
+    #   InvalidURLSubmission ():    Enters in an expired URL into the text ara and submits it
+    #
+    def test_InvalidURLSubmission (self):
+        driver = self.InitializeBrowserStackConnection ("Test an Invalid URL")
+        
+        # Navigate to page
+        driver.get ("http://localhost:5000/")
+
+        # Find textarea and input expired URL
+        driver.find_element_by_name ("text").click ()
+        driver.find_element_by_name ("text").send_keys ("http://moss.stanford.edu/results/xyz")
+
+        # Submit form data
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
+
+        # Assertion
+        self.assertEqual ("Uh Oh!", driver.find_element_by_xpath("//h1").text)
+
+
+
+
+
+
+
+    #
     #   SingleURLCheckUsingAllURLsButton ():    Enters in a valid URL and submits using the 'All URLs' button to confirm data entry
     #
     def test_SingleURLCheckUsingAllURLsButton (self):
@@ -57,11 +82,11 @@ class TopTenStudentsTest (FrontEndConfig.FrontEndTestSuite):
         driver.find_element_by_name ("text").send_keys (self.testURL)
 
         # Submit form data
-        driver.find_element_by_xpath("//input[@value='Submit']").click()
+        driver.find_element_by_xpath("//input[@value='Submit']").click ()
 
         # Select 'All URLs' button and submit
-        driver.find_element_by_xpath("(//input[@name='selection'])[2]").click()
-        driver.find_element_by_xpath("//input[@value='Submit']").click()
+        driver.find_element_by_xpath ("(//input[@name='selection'])[last()]").click ()
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
 
         # Assert that we successfully landed on the /moss page when submitting in this way.
         self.assertEqual (driver.current_url, 'http://localhost:5000/moss')
@@ -79,14 +104,36 @@ class TopTenStudentsTest (FrontEndConfig.FrontEndTestSuite):
 
         # Find textarea, input a collection of URLs, and submit form data
         driver.find_element_by_name ("text").click ()
-        driver.find_element_by_name("text").send_keys('\n'.join (self.testURLGroup))        
-        driver.find_element_by_xpath("//input[@value='Submit']").click()
+        driver.find_element_by_name ("text").send_keys('\n'.join (self.testURLGroup))        
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
 
         # Select the second item in the list of submitted URLs and submit form data again
-        driver.find_element_by_xpath("(//input[@name='selection'])[2]").click()
-        driver.find_element_by_xpath("//input[@value='Submit']").click()
+        driver.find_element_by_xpath ("(//input[@name='selection'])[2]").click ()
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
 
-        # NOTE:   We need to change this to verify that the assignments are all the same 
+        # Assert that we successfully landed on the /moss page when submitting in this way
+        self.assertEqual (driver.current_url, 'http://localhost:5000/moss')
+
+
+
+
+    #
+    #   MultipleURLsProvidedAllChosen ():   Enters in multiple URLs but only selects one URL to investigate
+    #
+    def test_MultipleURLsProvidedAllChosen (self):
+        driver = self.InitializeBrowserStackConnection ("Test an Expired URL")
+        
+        # Navigate to page
+        driver.get ("http://localhost:5000/")
+
+        # Find textarea, input a collection of URLs, and submit form data
+        driver.find_element_by_name ("text").click ()
+        driver.find_element_by_name ("text").send_keys('\n'.join (self.testURLGroup))        
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
+
+        # Select the last item in the list of submitted URLs and submit form data again
+        driver.find_element_by_xpath ("(//input[@name='selection'])[last()]").click ()
+        driver.find_element_by_xpath ("//input[@value='Submit']").click ()
 
         # Assert that we successfully landed on the /moss page when submitting in this way
         self.assertEqual (driver.current_url, 'http://localhost:5000/moss')
